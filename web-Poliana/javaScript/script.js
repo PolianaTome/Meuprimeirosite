cb29997fbed176b6a9465684090926ff2366d561
PolianaTome <105980022+PolianaTome@users.noreply.github.com>
2022-05-21 12:04:48 -0400

    Add files via upload

---
+++ web-Poliana/javaScript/script.js
@@ -0,0 +1,25 @@
+function verificar(){
+    var data = new date()
+    var ano = data.getUTFullYear()
+    var fAno = document.getElementById(txtano)
+    var res = document.getElementById(res)
+
+}
+if(fAno.value.lengt = 0 || Number(fAno.value)>ano){
+    window.alert([ERRO] verifique os dados e tente novamente)
+} else{
+    var fsex = Document.getElementById(radsex)
+    var idade = ano - Number (fAno.value)
+    var = genero = 
+    var emoji = document.createElement(emoji)
+    emoji.fSexAttribute(id, emoji)
+    if(fsex[0].checked){
+        genero = Masculino
+        if(idade >= 0 && idade < 10){
+//criança//
+
+
+        }
+    }
+    
+}
